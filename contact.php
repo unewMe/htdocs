1d5e245ee377921c5bcf05c387a0aba4776c3254
Dawid Chudzicki <chudzickid@gmail.com>
2023-09-01 00:52:03 +0200

    -a

---
+++ contact.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Akademia Korepetycji</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/variables.css" rel="stylesheet">
+
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <style>
+    /* Dodany styl do formularza */
+    .form-container {
+      background-color: #fff;
+      border-radius: 10px;
+      padding: 40px;
+      box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+      width: 100%;
+      max-width: 1000px;
+      margin: 0 auto;
+    }
+
+    .form-container h2 {
+      font-size: 32px;
+      margin-bottom: 20px;
+    }
+
+    .form-group {
+      margin-bottom: 20px;
+    }
+
+    .form-group label {
+      font-size: 18px;
+      font-weight: bold;
+      color: #333;
+      display: block;
+      margin-bottom: 10px;
+    }
+
+    .form-group input[type="text"],
+    .form-group input[type="email"] {
+      width: 100%;
+      padding: 10px;
+      border: 1px ridge #ccc;
+      border-radius: 4px;
+      font-size: 16px;
+    }
+
+    .form-group input[type="submit"] {
+      background-color: #ccc;
+      color: #black;
+      border: none;
+      padding: 12px 20px;
+      border-radius: 4px;
+      font-size: 16px;
+      cursor: pointer;
+      transition: background-color 0.3s;
+    }
+
+    .container-choices {
+        background-color: #ffffff;
+        border-radius: 10px;
+        padding: 40px;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+        width: 100%;
+        max-width: 400px;
+    }
+
+    .form-group select {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px; }
+
+    
+.course-buttons {
+    background-color: #;
+    width: 250px;
+    padding: 10px 20px;
+    
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+  .form-group button.active {
+    background-color: #ccc;
+  }
+
+  </style>
+
+</head>
+
+<body>
+
+<header id="header" class="header fixed-top" data-scrollto-offset="0">
+    <div class="container-fluid d-flex align-items-center justify-content-between">
+      <pev></pev>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+        <li><a class="nav-link scrollto" href="aplikuj.php">APLIKUJ</a></li>
+        <li><a class="nav-link scrollto" href="index.php">O NAS</a></li> 
+        <li><a class="nav-link scrollto" href="contact.php">KONTAKT</a></li>         
+        </ul>
+      </nav>
+
+    </div>
+  </header><!-- End Header -->
+
+  <section id="hero-fullscreen" class="hero-fullscreen d-flex align-items-center">
+    <div class="container d-flex flex-column align-items-center position-relative" data-aos="zoom-out">
+      <h2>Dlaczego warto wybrać naszą Akademię Korepetycji?</h2>
+      <div class="form-container">
+      <form action="process_form.php" method="post">
+            <div class="tile-container">
+            <p>
+                <br>📚 Indywidualne podejście: Zapewniamy spersonalizowane korepetycje dostosowane do Twoich potrzeb i poziomu nauki. Nasi doświadczeni nauczyciele pomogą Ci zrozumieć trudne zagadnienia w sposób, który będzie dla Ciebie klarowny i przystępny.</br>
+
+                <br>🚀 Skuteczna nauka: Nasze metody nauczania opierają się na sprawdzonych strategiach i technikach, które pomogą Ci szybko opanować materiał i utrwalić go na dłużej. Nie trać czasu na nudne powtórki – z nami nauka stanie się pasjonująca przygodą!</br>
+
+                <br>💡 Wsparcie na każdym etapie: Niezależnie od tego, czy potrzebujesz pomocy z konkretnym zadaniem, czy chcesz pracować nad ogólnym rozwojem, jesteśmy tu, aby Ci pomóc. Nasza ekipa chętnie odpowie na Twoje pytania i pomoże rozwiązać wszelkie trudności.</br>
+
+                <br>🌐 Przyjazna platforma online: Nauka powinna być wygodna i dostępna w dowolnym miejscu i czasie. Nasza platforma umożliwia zdalny dostęp do korepetycji, dzięki czemu możesz uczyć się bez względu na swój grafik.</br>
+
+                <br>🏆 Sukces, który przetrwa: Dążymy do tego, aby nie tylko pomóc Ci w osiągnięciu krótkoterminowych celów edukacyjnych, ale także wyposażyć Cię w umiejętności, które posłużą Ci przez całe życie. To inwestycja w przyszłość!</br>
+
+                <br>Nie czekaj – teraz jest najlepszy moment, aby rozpocząć swoją edukacyjną podróż z naszą Akademią Korepetycji. Zdobądź pewność siebie, czerp radość z nauki i otwórz drzwi do nieograniczonych możliwości! Skontaktuj się z nami już dziś i zacznij pracę nad swoim sukcesem razem z nami.</br>
+
+                <br>Zapraszamy serdecznie,</br>
+                <br>Zespół Akademii Korepetycji</br>
+            </p>
+            <a class="apply-button" href="index.php">Powrót</a>
+            </div>
+          </form>
+      </div>
+    </div>
+  </section>
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/js/main.js"></script>
+  <script>
+        const roleButtons = document.querySelectorAll(".course-buttons[data-role]");
+        const levelButtons = document.querySelectorAll(".course-buttons[data-level]");
+        const schoolButtons = document.querySelectorAll(".course-buttons[data-school]");
+        const selectedRoleInput = document.querySelector("#selectedRole");
+        const selectedSchoolInput = document.querySelector("#selectedSchool");
+        const selectedLevelInput = document.querySelector("#selectedLevel");
+
+        roleButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                roleButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedRoleInput.value = button.getAttribute("data-role");
+            });
+        });
+
+        schoolButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                schoolButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedSchoolInput.value = button.getAttribute("data-school");
+            });
+        });
+
+        levelButtons.forEach(button => {
+            button.addEventListener("click", () => {
+                levelButtons.forEach(btn => btn.classList.remove("active"));
+                button.classList.add("active");
+                selectedLevelInput.value = button.getAttribute("data-level");
+            });
+        });
+  </script>
+</body>
+
+</html>
